df27b77b206c33cfb54d79bde10c5829e2ec09ed
yosh <ilyass.mejdoub@pm.me>
2025-10-18 16:24:14 +0000

    strugling with gradio

--- src/gradio_demo/app/main.py
+++ src/gradio_demo/app/main.py
@@ -1,6 +1,8 @@
 import gradio as gr
 import numpy as np
 import torch 
+from pathlib import Path
+from datasets import load_dataset
 from gradio_demo.model.sketch_images.cnn import CNN
 
 
@@ -13,34 +15,38 @@ params = {
     'n_classes': n_classes
 }
 model = CNN(**params)
-PATH = '../../model/sketch_images/weights/cnn.pth'
-model.load_state_dict(torch.load(PATH))
-# model = torch.load("../model/sketch_images/weights/cnn.pth", map_location="cpu")
+ROOT = Path(__file__).resolve().parents[2]
+PATH = ROOT / "gradio_demo" / "model" / "sketch_images" / "weights" / "cnn.pth"
+model.load_state_dict(torch.load(PATH, map_location='cpu'))
 model.eval()
 
-# def predict(img):
-#     # img = img.convert("L").resize((28, 28))
-#     # arr = np.array(img) / 255.0
-#     # tensor = torch.tensor(arr).unsqueeze(0).unsqueeze(0).float()
-#     # with torch.no_grad():
-#     #     out = model(tensor)
-#     # return int(torch.argmax(out))
-#     return 'djg'
+# utils
+dataset = load_dataset("Xenova/quickdraw-small")
+train_dataset = dataset['train']
+names = train_dataset.features['label'].names
 
-# demo = gr.Interface(
-#     fn=predict,
-#     inputs=gr.Sketchpad(crop_size=(256,256), label="Draw a Digit a khawa dialo"),
-#     outputs=gr.Label(num_top_classes=1),
-#     title="Digit Classifier"
-# )
+def predict(input_image):
+    img = input_image['composite']
+    # print(f'img {img.shape} type {type(img)}')
+    img = img / 255.0
+    img = np.resize(img, (28, 28))
+    img = torch.from_numpy(img)
+    # print(f'type {type(img)} shape {img.shape}')
+    img = img.unsqueeze(0).unsqueeze(0).to(torch.float32)
+    # print('-'*50)
+    # print(f'type {type(img)} shape {img.shape} dtype {img.dtype}')
+    # print('-'*50)
+    with torch.no_grad():
+        out = model(img)
+    idx = torch.argmax(out).item()
+    return names[idx]
 
-# # iface.launch(server_name="0.0.0.0", server_port=8000)
-# # iface.launch(server_name='0.0.0.0', root_path="/gradio")
-# # iface.launch()
+demo = gr.Interface(
+    fn=predict,
+    inputs=gr.Sketchpad(label="Draw a Digit a khawa diali", image_mode='L'),
+    outputs=gr.Label(num_top_classes=1),
+    title="Digit Classifier"
+)
 
 if __name__ == "__main__":
-    # gr.serve(demo, reload=True)
-    # demo.launch()
-    a = torch.rand(size=(1, 1, 28, 28))
-    out = model(a)
-    print(out)
+    demo.launch()
