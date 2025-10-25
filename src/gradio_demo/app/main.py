10013cd581794c7673eb05c5bc51cd89d5fcc567
yosh <ilyass.mejdoub@pm.me>
2025-10-25 12:40:55 +0000

    working version of sketch model with gradio interface

--- src/gradio_demo/app/main.py
+++ src/gradio_demo/app/main.py
@@ -1,6 +1,8 @@
 import gradio as gr
 import numpy as np
 import torch 
+import torch.nn.functional as F
+import torchvision.transforms as T
 from pathlib import Path
 from datasets import load_dataset
 from gradio_demo.model.sketch_images.cnn import CNN
@@ -24,18 +26,39 @@ model.eval()
 dataset = load_dataset("Xenova/quickdraw-small")
 train_dataset = dataset['train']
 names = train_dataset.features['label'].names
+transform = T.Compose([
+    T.ToTensor(),                            # (1, H, W), values in [0, 1], white=1 black=0
+    # T.Lambda(lambda x: x/255.0), 
+    T.Lambda(lambda x: 1.0 - x),             # invert -> white=0, black=1 ✅
+    T.Resize((28, 28), interpolation=T.InterpolationMode.BILINEAR),
+    # T.Normalize((0.5,), (0.5,))            # optional if your model expects [-1, 1]
+])
+
+# img_tensor = transform(img)
 
 def predict(input_image):
     img = input_image['composite']
     # print(f'img {img.shape} type {type(img)}')
-    img = img / 255.0
-    img = np.resize(img, (28, 28))
-    img = torch.from_numpy(img)
-    # print(f'type {type(img)} shape {img.shape}')
-    img = img.unsqueeze(0).unsqueeze(0).to(torch.float32)
-    # print('-'*50)
-    # print(f'type {type(img)} shape {img.shape} dtype {img.dtype}')
-    # print('-'*50)
+    # img = img / 255.0
+    # img = torch.from_numpy(img)
+    # print(f'OG image model type {img}')
+    # img = img.unsqueeze(0)
+    # resize = T.Resize((28, 28), interpolation=T.InterpolationMode.BICUBIC, antialias=True)
+    # img = resize(img)
+    img = transform(img)
+    print('-'*50)
+    print('-'*50)
+    print(f'type {type(img)} shape {img.shape}')
+    print('-'*50)
+    print('-'*50)
+    print('-'*50)
+    img = img.unsqueeze(0).to(torch.float32)
+    print('-'*50)
+    print(f'before model type {type(img)} shape {img.shape} dtype {img.dtype}')
+    print('-'*50)
+    print('-'*50)
+    print(f'image model type {img}')
+    print('-'*50)
     with torch.no_grad():
         out = model(img)
     idx = torch.argmax(out).item()
